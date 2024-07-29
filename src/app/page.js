d78c818d2dca2135449b1255d9e9372f6cd25dd6
daniekahora <daniekahora@gmail.com>
2024-07-29 10:54:17 +0300

    Removed some items from the hero section

--- src/app/page.js
+++ src/app/page.js
@@ -31,7 +31,7 @@ export default function Home() {
       <div className="mt-8">
         {posts ? (
           <>
-            {posts.length > 0 && <h3 className="border-1 bg-orange-300 border-primary px-6 py-2 rounded-md text-white font-semibold  hover:text-white transition-all duration-300 max-w-max">
+            {posts.length > 0 && <h3 className="border-1 bg-orange-300 border-primary px-6 mx-5 py-2 rounded-md text-white font-semibold  hover:text-white transition-all duration-300 max-w-max">
                   Featured Post
               </h3>}
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
