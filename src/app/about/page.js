03d681a97b723e4465997a7a8d5272109890439c
daniekahora <daniekahora@gmail.com>
2024-11-22 21:02:41 +0300

    updated page data

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,26 +2,27 @@ import React from 'react'
 
 const page = () => {
   return (
-    <section class=" py-12 px-6 md:px-16 lg:px-24 h-full">
-  <div class="max-w-5xl mx-auto text-center">
-    <h1 class="text-3xl font-bold text-gray-800 md:text-4xl">
-      Welcome to <span class="text-indigo-600">MAJIRANI WORLD</span>
+    <section className="bg-gray-100 py-12 px-6 md:px-16 lg:px-24">
+  <div className="max-w-5xl mx-auto text-center">
+    <h1 className="text-3xl font-bold text-gray-800 md:text-4xl">
+      Welcome to <span className="text-indigo-600">MAJIRANI WORLD</span>
     </h1>
-    <p class="mt-6 text-lg text-gray-700 leading-relaxed">
+    <p className="mt-6 text-lg text-gray-700 leading-relaxed">
       Your go-to destination for diverse, insightful, and engaging articles! We cover a wide range of topics, from 
-      <span class="font-semibold text-indigo-600"> lifestyle and wellness</span> to <span class="font-semibold text-indigo-600">technology</span>, 
-      <span class="font-semibold text-indigo-600">travel</span>, and everything in between.
+      <span className="font-semibold text-indigo-600">lifestyle and wellness</span> to <span className="font-semibold text-indigo-600">technology</span>, 
+      <span className="font-semibold text-indigo-600">travel</span>, and everything in between.
     </p>
-    <p class="mt-4 text-lg text-gray-700 leading-relaxed">
+    <p className="mt-4 text-lg text-gray-700 leading-relaxed">
       Our mission is to provide you with fresh perspectives, thought-provoking ideas, and practical advice through 
-      well-researched and expertly crafted content. Whether you're looking to stay informed, inspired, or entertained, 
-      you'll find something here that speaks to you.
+      well-researched and expertly crafted content. Whether you&apos;re looking to stay informed, inspired, or entertained, 
+      you&apos;ll find something here that speaks to you.
     </p>
-    <p class="mt-6 text-lg text-gray-700 leading-relaxed">
+    <p className="mt-6 text-lg text-gray-700 leading-relaxed">
       Explore our site and discover a world of knowledge, all in one place!
     </p>
   </div>
 </section>
+
 
   )
 }
